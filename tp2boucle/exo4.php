0ed6e631c71d4103b9859f135387825484763579
HugoDemont <hugodemont62@hotmail.fr>
2022-09-28 16:02:23 +0200

    Modif var

--- tp2boucle/exo4.php
+++ tp2boucle/exo4.php
@@ -1,20 +1,19 @@
-<?php 
+<?php
 include("../inputs.php");
-readVariables("nb1","op1");
+readVariables("nb1", "op1");
 
-    if($op1 == "*"){
-        for($m = 0; $m < 11; $m++){
-            $resul = $nb1*$m;
-            echo "<li> $nb1 $op1 $m = $resul</li>";
-        }
+if ($op1 == "*") {
+    for ($m = 0; $m < 11; $m++) {
+        $resul = $nb1 * $m;
+        echo "<li> $nb1 $op1 $m = $resul</li>";
     }
-        if($op1 == "+") {
-            for($m = 0; $m < 11; $m++){
-                $resul = $nb1+$m;
-                echo "<li> $nb1 $op1 $m = $resul</li>";
-            }
-        }
+}
+if ($op1 == "+") {
+    for ($m = 0; $m < 11; $m++) {
+        $resul = $nb1 + $m;
+        echo "<li> $nb1 $op1 $m = $resul</li>";
+    }
+}
 
 
 
-?> 
